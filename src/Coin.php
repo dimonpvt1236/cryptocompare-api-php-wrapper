7b909dbacf8173a153ac83a04a56bbe7c7d21dcc
Ciki <matula.m@gmail.com>
2017-12-04 16:36:17 +0100

    code style

--- src/Coin.php
+++ src/Coin.php
@@ -10,55 +10,64 @@ namespace Cryptocompare;
 
 class Coin extends CryptocompareApi
 {
-    /**
-     * @return bool|mixed - returns general info for all the coins available on the website.
-     */
-    public function getList($sign = false) {
-        $extraParams = $this->appplicationName;
-        $params = array(
-            "extraParams" => $extraParams,
-            "sign" => $sign
-        );
-        $r = $this->getRequest("public", "/data/all/coinlist", $params);
-        return $r;
-    }
 
-    /**
-     * @param string $fsym
-     * @param string $tsym
-     * @return bool|mixed
-     */
-    public function getSnapshot($fsym = "BTC", $tsym = "EUR") {
-        $params = array(
-            "fsym" => $fsym,
-            "tsym" => $tsym
-        );
-        $r = $this->getRequest("private", "/coinsnapshot", $params);
-        return $r;
-    }
+	/**
+	 * @return bool|mixed - returns general info for all the coins available on the website.
+	 */
+	public function getList($sign = false)
+	{
+		$extraParams = $this->appplicationName;
+		$params = array(
+			"extraParams" => $extraParams,
+			"sign" => $sign
+		);
+		$r = $this->getRequest("public", "/data/all/coinlist", $params);
+		return $r;
+	}
 
-    /**
-     * @param int $id
-     * @return bool|mixed
-     */
-    public function getSnapshotFullById($id = 0) {
-        $params = array(
-            "id" => $id,
-        );
-        $r = $this->getRequest("private", "/coinsnapshotfullbyid", $params);
-        return $r;
-    }
 
-    /**
-     * @param int $id
-     * @return bool|mixed
-     */
-    public function getSocialStats($id = 0) {
-        $params = array(
-            "id" => $id,
-        );
-        $r = $this->getRequest("private", "/socialstats", $params);
-        return $r;
-    }
+	/**
+	 * @param string $fsym
+	 * @param string $tsym
+	 * @return bool|mixed
+	 */
+	public function getSnapshot($fsym = "BTC", $tsym = "EUR")
+	{
+		$params = array(
+			"fsym" => $fsym,
+			"tsym" => $tsym
+		);
+		$r = $this->getRequest("private", "/coinsnapshot", $params);
+		return $r;
+	}
+
+
+	/**
+	 * @param int $id
+	 * @return bool|mixed
+	 */
+	public function getSnapshotFullById($id = 0)
+	{
+		$params = array(
+			"id" => $id,
+		);
+		$r = $this->getRequest("private", "/coinsnapshotfullbyid", $params);
+		return $r;
+	}
+
+
+	/**
+	 * @param int $id
+	 * @return bool|mixed
+	 */
+	public function getSocialStats($id = 0)
+	{
+		$params = array(
+			"id" => $id,
+		);
+		$r = $this->getRequest("private", "/socialstats", $params);
+		return $r;
+	}
+
 
 }
